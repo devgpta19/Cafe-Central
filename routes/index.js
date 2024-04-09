0f9f1457933b3752a654251a559d8b7cf6b94dd5
Devansh Gupta <devansh2002gupta@gmail.com>
2024-04-09 09:32:43 +0530

    Updated starters.ejs. mainCourses.ejs and index.ejs
    Updated the starters.ejs and mainCourses.ejs . The website's structure consists of a home page with two options: Starters and Main Course. Users can navigate between these options to explore different recipes according to the restro.
    
    - Home Page: The home page serves as the entry point for users and presents them with two options: Starters and Main Course.
    
    - Starters Page: This page showcases a collection of starter recipes.
    
    - Main Course Page: Similar to the Starters page, this page displays a variety of main course recipes.
    
    - Navigation: The website provides easy navigation between the home page, starters page, and main course page, allowing users to seamlessly explore different recipe categories.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require('./users');
+const productModel = require('./product');
+const cartModel = require('./cart');
+const cartProductModel = require('./cartProduct');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+
+router.get('/starters', function(req, res){
+  res.render('starters')
+})
+
+router.get('/mainCourse', function(req, res){
+  res.render('maincourse')
+})
 
 module.exports = router;
