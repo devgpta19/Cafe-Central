bd07f21320866b40396d9260411403d96cefd276
Devansh Gupta <devansh2002gupta@gmail.com>
2024-04-09 17:56:05 +0530

    Login And Signup with Authentication for Seller completed
    Utilizes Passport.js for user authentication, allowing sellers and buyers both to securely log in and sign up.  Sellers can add and manage their products through the application but buyers can browse products and make purchases.
    
    Passport.js is used as an authentication middleware for this application

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const userModel = require('./users');
-const productModel = require('./product');
-const cartModel = require('./cart');
-const cartProductModel = require('./cartProduct');
+
+const users = require('./users');
+const passport = require('passport');
+const localStrategy = require('passport-local');
+passport.use(new localStrategy(users.authenticate()));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',function (req, res, next) {
   res.render('index', { title: 'HOME' });
 });
 
@@ -18,11 +20,63 @@ router.get('/register', function (req, res) {
   res.render('register', { title: 'Register' });
 })
 
-router.get('/starters', function(req, res){
+
+router.post('/register', function (req, res) {
+  console.log(req.body);
+  
+  
+  var userData = new userModel({
+    username: req.body.username,
+    accountType: req.body.isSeller == 'on' ? "seller" : "buyer"  
+  })
+  userModel
+    .register(userData, req.body.password)
+    .then(function (registeredUser) {
+      passport.authenticate('local')(req, res, function () {
+        if(registeredUser.accountType === 'seller'){
+          res.redirect("/createProduct")
+          return;
+        }
+        res.render('index')
+      })
+    })
+})
+
+router.post('/login', passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login"
+}), function (req, res) {
+});
+
+router.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect('login');
+  });
+});
+
+function isloggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  else res.redirect("/login")
+}
+
+function isSeller(req,res,next){
+  if(req.user.accountType === 'seller') return next();
+  else res.redirect('/')
+}
+
+router.get('/createProduct', isloggedIn, isSeller, function(req,res){
+  res.render('createProduct', {title:'Create Product'})
+})
+
+router.get('/starters',isloggedIn, function (req, res) {
   res.render('starters', { title: 'Starters' })
 })
 
-router.get('/mainCourse', function(req, res){
+
+router.get('/mainCourse', function (req, res) {
   res.render('maincourse', { title: 'Main Course' })
 })
 
--- routes/product.js
+++ routes/product.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
     name: String,
     price: Number,
+    category:String,
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'users'
